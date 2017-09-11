eeb851b1e24f1a1b45e0740c84f8aa10020c75b2
Jason Ge <jiange@princeton.edu>
2017-09-11 11:23:31 -0400

    refactoring

--- src/solver/actnewton.cc
+++ src/solver/actnewton.cc
@@ -3,27 +3,11 @@
 
 namespace picasso {
 namespace solver {
-DMLC_REGISTRY_FILE_TAG(solver);
-
-// model parameters
-struct SpLinearModelParam: public dmlc::Parameter<SpLinearModelParam> {
-  // feature dimension
-  unsigned dim_feature;
-
-  SpLinearModelParam() {
-    std::memset(this, 0, sizeof(SpLinearModelParam));
-  }
-
-  DMLC_DECLARE_PARAMETER(SpLinearModelParam) {
-    DMLC_DECLARE_FIELD(dim_feature).set_lower_bound(0)
-      .describe("Dimension of features.");
-  }
-};
-
 enum RegType {L1, SCAD, MCP};
 
 // training parameters
-struct ActNewtonTrainParam: public dmlc::Parameter<ActNewtonTrainParam> {
+class ActNewtonTrainParam {
+public:
   /*! number of regularization parameters */
   unsigned num_lambda;
 
@@ -42,40 +26,74 @@ struct ActNewtonTrainParam: public dmlc::Parameter<ActNewtonTrainParam> {
   /*! precision of optimization */
   double prec;
 
-  DMLC_DECLARE_PARAMETER(ActNewtonTrainParam) {
-    DMLC_DECLARE_FIELD(num_lambda).set_lower_bound(0).set_default(100)
-      .describe("number of parameters on the regularization path.");
-    DMLC_DECLARE_FIELD(target_lambda).set_lower_bound(0).set_default(1e-3)
-      .describe("the smallest parameter on the regularization path");
-    DMLC_DECLARE_FIELD(reg_type).set_default(L1)
-      .describe("the type of regularization");
-    DMLC_DECLARE_FIELD(num_relaxation_round).set_lower_bound(0).set_default(3)
-      .describe("number of rounds for multistage convex relaxation.");
-    DMLC_DECLARE_FIELD(prec).set_lower_bound(0).set_default(1e-4)
-      .describe("precision of optimization solution.")
+  std::vector<double> lambdas;
+
+  ActiveNewtonTrainParam() {
+    num_lambda = 100;
+    target_lambda = 1e-6;
+    reg_type = L1;
+    reg_gamma = 3.0;
+    num_relaxation_round = 3;
+    prec = 1e-4;
+    lambdas.clear();
+  }
+
+  void configure(const std::vector<std::pair<std::string, std::string> >& cfg){
+    for (auto iter = cfg.begin(); iter != cfg.end(); iter++){
+      if (iter.first == "nlambda")
+        num_lambda = stoi(iter.second)
+      else if (iter.first == "target_lambda")
+        target_lambda = stof(iter.second)
+      else if (iter.first == "reg_type"){
+        if (iter.second == "L1")
+          reg_type = L1;
+        else if (iter.second == "SCAD")
+          reg_type = SCAD;
+        else if (iter.second == "MCP")
+          reg_type = MCP;
+        else 
+          // throw exception
+      }
+      else 
+        // TODO
+    }
+  }
+
+  void set_lambdas(const double * lambda_path, int n) {
+    lambdas.resize(n);
+    for (int i = 0; i < n; i++)
+      lambdas[i] = lambda_path[i];
+    num_lambda = lambdas.size();
+    target_lambda = lambdas[num_lambda-1];
   }
 }
 
 class ActNewtonSolver {
 private:
-  ActNewtonTrainParam m_param;
+  const ActNewtonTrainParam m_param;
   const ObjFunction * m_obj;
-public:
-  ActNewtonSolver(ObjFunction * obj):m_obj(obj) {}
 
-  void configure(const std::vector<std::pair<std::string, std::string> >& cfg){
-    m_param.init(cfg);
-  }
+public:
+  ActNewtonSolver(ObjFunction * obj, ActNewtonTrainParam param):
+    m_obj(obj), m_param(param) {}
 
   void solve(ObjFunction* obj){
     std::vector<double> & gr = obj->grad();
+    double dev_null = obj->eval(); // initial un-penalized fvalue
+
     const std::vector<double> & lambda_path = param.lambda_path();
+
+    std::vector<int> active_set;
+    active_set.resize(d);
     
-    model_before_relaxation = obj->model_params();
+    // model parameters on the master branch
+    ModelParam model_master = obj->model_params();
+
     obj->init_active_set();
+
     for (auto lambda_iter = lambda_path.begin(); 
           lambda_iter != lambda_path.end(); lambda_iter++){
-      obj->set_model_params(model_before_relaxation);  
+      obj->set_model_params(model_master);  
 
       if (lambda_iter != lambda_path.begin())
         obj->reset_active_set();
