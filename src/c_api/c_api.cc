f76a5260239600f66d0a0fc67cc7789963f09a97
Jian Ge <jiange@princeton.edu>
2017-09-12 02:30:34 -0400

    refactor

---
+++ src/c_api/c_api.cc
@@ -0,0 +1,19 @@
+#include <picasso/c_api.h>
+
+using namespace picasso;
+
+extern "C" SolveLogisticRegression() {
+
+}
+
+extern "C" SolvePoissonRegression() {
+
+} 
+
+extern "C" SolveSqrtLinearRegression() {
+
+}
+
+extern "C" SolveLinearRegression() {
+
+}
